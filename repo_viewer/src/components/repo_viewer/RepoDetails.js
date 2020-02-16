544af17b6a63a805f6e84f416fb0c0c9f5fbd012
wini <imbatman@wp.pl>
2020-02-16 18:32:32 +0100

    VER-3 - init React panel

---
+++ repo_viewer/src/components/repo_viewer/RepoDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RepoDetails extends React.Component {
+    render(){
+        return (
+            <>
+                Here put some details
+            </>
+        );
+    }
+}
+
+export default RepoDetails;
---
+++ repo_viewer/src/components/repo_viewer/RepoHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RepoHeader extends React.Component {
+    render(){
+        return (
+            <>
+                It's the header
+            </>
+        );
+    }
+}
+
+export default RepoHeader;

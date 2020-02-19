d906bdee0d304ac0e8d0d3e441efa016309d44cb
wini <imbatman@wp.pl>
2020-02-19 01:10:38 +0100

    VER-4 rewrting sort function to redux

--- repo_viewer/src/redux/configureStore.js
+++ repo_viewer/src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import {applyMiddleware, combineReducers, compose, createStore} from "redux";
-import {reposDetailsReducer, reposReducer} from "../components/repo/reducers";
+import {reposDetailsReducer, reposReducer, sortReposByReducer} from "../components/repo/reducers";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 
@@ -8,6 +8,7 @@ export const configureStore = () => {
         combineReducers({
             repos: reposReducer,
             reposDetails: reposDetailsReducer,
+            sortReposBy: sortReposByReducer
         }),
         composeWithDevTools(applyMiddleware(thunk),
     ));
